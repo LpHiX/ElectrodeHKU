9835ab50ca48f542c8711f992686c125a008e8c0
LpHiX <martin.leung.ht@gmail.com>
2022-08-24 18:31:17 +0800

    Starting heart UI

---
+++ Assets/Scripts/ElectrodeScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectrodeScript : MonoBehaviour
+{
+    public int electrodeNumber;
+    // Start is called before the first frame update
+}
---
+++ Assets/Scripts/SocketShowScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocketShowScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject visualizer;
+    // Start is called before the first frame update
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        visualizer.SetActive(true);
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        visualizer.SetActive(false);
+    }
+    */
+}
